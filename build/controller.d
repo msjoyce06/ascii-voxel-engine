5eb7142ccf46ae85f227cc7659b5c45f948da089
msjoyce <michaeljoyce06@gmail.com>
2026-03-13 12:50:47 -0400

    stop tracking build directory

--- build/controller.d
+++
@@ -1,4 +0,0 @@
-build/controller.o: src/controller.c include/controller.h \
- include/vectors.h
-include/controller.h:
-include/vectors.h:
--- build/render.d
+++
@@ -1,7 +0,0 @@
-build/render.o: src/render.c include/render.h include/vectors.h \
- include/world.h include/controller.h include/args.h
-include/render.h:
-include/vectors.h:
-include/world.h:
-include/controller.h:
-include/args.h:
